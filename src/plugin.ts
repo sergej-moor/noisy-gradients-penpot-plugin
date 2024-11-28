6f75bd32f88206dfad97f7f8248e7980d2e6299e
Sergej Moor <sergejmoor01@gmail.com>
2024-11-28 21:19:49 +0100

    renaming

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,7 +1,7 @@
 import type { PluginMessageEvent } from "./types";
 
 // Initialize plugin with slightly less height since canvas is now more compact
-penpot.ui.open("Noisy Hues", `?theme=${penpot.theme}`, {
+penpot.ui.open("Noisy Gradients", `?theme=${penpot.theme}`, {
   width: 264,
   height: 540,
 });
